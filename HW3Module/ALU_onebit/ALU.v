5e27ddbbd27a7683d5f4452ba33f4392d5a21fbf
Austin-Crabtree <ac732213@ohio.edu>
2017-03-16 11:55:08 -0400

    Added full adder module and cout to the adder

--- HW3Module/ALU_onebit/ALU.v
+++ HW3Module/ALU_onebit/ALU.v
@@ -1,4 +1,4 @@
-module OneBitALU(a, b, cin, less, op, result, g, p, set);
+module OneBitALU(a, b, cin, less, op, result, g, p, set, cout);
   input a, b, cin; // Inputs to the one-bit ALU, "cin" is the carry-in bit.
   input [2:0] op;  // 3-bit operation code.op[2] is the "binv".op[0] is the
                    // least significant bit.
@@ -9,13 +9,15 @@ module OneBitALU(a, b, cin, less, op, result, g, p, set);
   output g, p, set;// Generate and propagate signals that are to be used by the
                    // CLA unit.output set;
                    // This is the "sum" output of the full-adder.
+  output cout;
 
   wire bmux;
   mux2to1 bmux2to1(b, ~b, op[2], bmux);
 
   assign p = a | bmux;
   assign g = a & bmux;
-  assign set = (~a & ~bmux & cin) + (~a & bmux & ~cin) + (a & ~bmux & ~cin) + (a & bmux & cin);
+
+  Adder sumadder(a, bmux, cin, set, cout);
 
   mux4to1 resultmux4to1(g, p, set, less, op[1], op[0], result);
 
--- HW3Module/ALU_onebit/ALU_tb.v
+++ HW3Module/ALU_onebit/ALU_tb.v
@@ -5,16 +5,16 @@ module OneBitALU_tb();
   reg [2:0] op;
 
   // Outputs
-  wire result, g, p, set;
+  wire result, g, p, set, cout;
 
   initial begin
     //This is needed for iverilog and gtkwave
     $dumpfile("wave");
     $dumpvars(0, test);
     //--------------------------------------
-    $display("time\ta\tb\tcin\tless\top\tresult\tg\tp\tset");
-    $monitor("%g\t%b\t%b\t%b\t%b\t%b\t%b\t%b\t%b\t%b",
-    $time, a, b, cin, less, op, result, g, p, set);
+    $display("time\ta\tb\tcin\tless\top\tresult\tg\tp\tset\tcout");
+    $monitor("%g\t%b\t%b\t%b\t%b\t%b\t%b\t%b\t%b\t%b\t%b",
+    $time, a, b, cin, less, op, result, g, p, set, cout);
 
     ///////////////////
     //TEST 1
@@ -39,7 +39,20 @@ module OneBitALU_tb();
 
     // Wait 10 ns for global reset to finish
     #10;
+
+    ///////////////////
+    //TEST 2
+    // Initialize Inputs
+    a = 1;
+    b = 1;
+    cin = 0;
+    less = 1;
+    op = 3'b001;
+
+
+    // Wait 10 ns for global reset to finish
+    #10;
   end
 
-  OneBitALU test(a, b, cin, less, op, result, g, p, set);
+  OneBitALU test(a, b, cin, less, op, result, g, p, set, cout);
 endmodule
