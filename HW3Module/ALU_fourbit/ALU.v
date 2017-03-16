1fb7a5dd6d024d5604f5682be768b687c30c5e19
JTEnglish <je787413@ohio.edu>
2017-03-16 16:24:20 -0400

    it compiles, but I think the overflow detection module needs to be changed a bit.

--- HW3Module/ALU_fourbit/ALU.v
+++ HW3Module/ALU_fourbit/ALU.v
@@ -10,13 +10,21 @@ output set;         // This is the set output of the most significant ALU block
 output overflow;    // This bit indicatesthat an overflow has occurred.
                     // (Ignores what operation is chosen for ALU)
 
-OneBitALU alu1bit_0(a, b, cin, less, op, result, g, p, set, cout);
-OneBitALU alu1bit_1(a, b, cin, less, op, result, g, p, set, cout);
-OneBitALU alu1bit_2(a, b, cin, less, op, result, g, p, set, cout);
-OneBitALU alu1bit_3(a, b, cin, less, op, result, g, p, set, cout);
+wire p0, p1, p2, p3;
+wire g0, g1, g2, g3;
+wire C1, C2, C3;
+wire set0, set1, set2, set3;
+wire cout0, cout1, cout2, cout3;
+wire less;
+assign less = 1'b0;
 
-CLA cla(g0, p0, g1, p1, g2, p2, g3, p3, cin, C1, C2, C3, C4, G, P);
+OneBitALU alu1bit_0(a[0], b[0], op[2], set3, op, result[0], g0, p0, set0, cout0); //where do these couts need to go?
+OneBitALU alu1bit_1(a[1], b[1], C1, less, op, result[1], g1, p1, set1, cout1);
+OneBitALU alu1bit_2(a[2], b[2], C2, less, op, result[2], g2, p2, set2, cout2);
+OneBitALU alu1bit_3(a[3], b[3], C3, less, op, result[3], g3, p3, set3, cout3);
 
-overflowdetect overflow(cin, cout, v);
+CLA cla(g0, p0, g1, p1, g2, p2, g3, p3, op[2], C1, C2, C3, cout, G, P);
+
+overflowdetect ovrflw(cin, cout, v); //we might need to change the parameters (diagram has 3 inputs, one output)
 
 endmodule
