f94f461078e9fa0a41526fb7140ad96e274b45cc
Austin-Crabtree <ac732213@ohio.edu>
2017-03-21 17:52:22 -0400

    Added files to folder

---
+++ HW4Module/ALU_16bit/CLA.v
@@ -0,0 +1,17 @@
+ module CLA(g0, p0, g1, p1, g2, p2, g3, p3, cin, C1, C2, C3, C4, G, P);
+  // inputs g and p are 0 -> LSB and 3 -> MSB
+  input wire g0, p0, g1, p1, g2, p2, g3, p3; // Gen and Prop signals for each bit
+	input wire cin; //Carry in input
+	output wire C1, C2, C3, C4; //Carry bits computed by CLA
+	output wire G, P; //Block Gen and Block Prop used by CLA's at a higher level
+
+  //carry output assignments
+  assign C1 = g0 | (p0 & cin);
+  assign C2 = g1 | (p1 & g0) | (p1 & p0 & cin);
+  assign C3 = g2 | (p2 & g1) | (p2 & p1 & g0) | (p2 & p1 & p0 & cin);
+  assign C4 = g3 | (p3 & g2) | (p3 & p2 & g1) | (p3 & p2 & p1 & g0) | (p3 & p2 & p1 & p0 & cin);
+
+  //super generate and super propagate assignments
+  assign G = g3 | (p3 & g2) | (p3 & p2 & g1) | (p3 & p2 & p1 & g0);
+  assign P = p3 & p2 & p1 & p0;
+endmodule
---
+++ HW4Module/ALU_16bit/fourbitALU.v
@@ -0,0 +1,32 @@
+module FourBitALU(a, b, op, result, cout, G, P, set, overflow);
+input [3:0] a, b;   // Inputs to the one-bit ALU.
+input [2:0] op;     // 3-bit operation code.op[2] is the "binv".op[0] is the
+                    // least significant bit.
+output [3:0] result;// The result of the ALU(depends on the operation that
+                    // is chosen)
+output cout;        // Carry-out bit of the ALU
+output G, P;        // Block generate and propagate of the four-bit ALU
+output set;         // This is the set output of the most significant ALU block
+output overflow;    // This bit indicatesthat an overflow has occurred.
+                    // (Ignores what operation is chosen for ALU)
+
+wire p0, p1, p2, p3;
+wire g0, g1, g2, g3;
+wire C1, C2, C3;
+wire set0, set1, set2, set3;
+wire cout0, cout1, cout2, cout3;
+wire less;
+assign less = 1'b0;
+
+OneBitALU alu1bit_0(a[0], b[0], op[2], set3, op, result[0], g0, p0, set0, cout0); //where do these couts need to go?
+OneBitALU alu1bit_1(a[1], b[1], C1, less, op, result[1], g1, p1, set1, cout1);
+OneBitALU alu1bit_2(a[2], b[2], C2, less, op, result[2], g2, p2, set2, cout2);
+OneBitALU alu1bit_3(a[3], b[3], C3, less, op, result[3], g3, p3, set3, cout3);
+
+assign set = result[3];
+
+CLA cla(g0, p0, g1, p1, g2, p2, g3, p3, op[2], C1, C2, C3, cout, G, P);
+
+overflowdetect ovrflw(C3, cout, overflow);
+
+endmodule
