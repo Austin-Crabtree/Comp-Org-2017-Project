5b663ce2323398e43e6a1212033fc0084360cf84
JTEnglish <je787413@ohio.edu>
2017-03-27 12:19:30 -0400

    fixed the 16bit alu problem, there are more issues in 4bit alu now

--- HW4Module/ALU_32bit/16bitALU.v
+++ HW4Module/ALU_32bit/16bitALU.v
@@ -10,15 +10,16 @@ module sixteenBitALU(a, b, cin, less, op, result, cout, set, zero, G, P, overflo
   wire g2, p2;
   wire g3, p3;
   wire null_cout;
+  wire null_set;
 
   wire C1, C2, C3;
 
   wire [3:0] zeros;
 
-  FourBitALU alu_0(a[3:0], b[3:0], cin, op, result[3:0], null_cout, g0, p0, set, overflow,zeros[0], 1'b0);
-  FourBitALU alu_1(a[7:4], b[7:4], C1, op, result[7:4], null_cout, g1, p1, set, overflow, zeros[1], 1'b0);
-  FourBitALU alu_2(a[11:8], b[11:8], C2, op, result[11:8], null_cout, g2, p2, set, overflow, zeros[2], 1'b0);
-  FourBitALU alu_3(a[15:12], b[15:12], C3, op, result[15:12], cout, g3 p3, set, overflow, zeros[3], 1'b0);
+  FourBitALU alu_0(a[3:0], b[3:0], cin, op, result[3:0], null_cout, g0, p0, null_set, overflow,zeros[0], less);
+  FourBitALU alu_1(a[7:4], b[7:4], C1, op, result[7:4], null_cout, g1, p1, null_set, overflow, zeros[1], 1'b0);
+  FourBitALU alu_2(a[11:8], b[11:8], C2, op, result[11:8], null_cout, g2, p2, null_set, overflow, zeros[2], 1'b0);
+  FourBitALU alu_3(a[15:12], b[15:12], C3, op, result[15:12], null_cout, g3, p3, set, overflow, zeros[3], 1'b0);
 
   CLA CLA1(g0, p0, g1, p1, g2, p2, g3, p3, cin, C1, C2, C3, cout, G, P);
 
