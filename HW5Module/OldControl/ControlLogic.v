620142570ff738a8fcf654f3140a8143520b468e
Austin-Crabtree <ac732213@ohio.edu>
2017-04-04 22:09:37 +0000

    Error bullshittery that means nothing in life

--- HW5Module/OldControl/ControlLogic.v
+++ HW5Module/OldControl/ControlLogic.v
@@ -1,27 +1,37 @@
 module OldControlLogic (opcode, ALUsrc, ALUop, RegDst, MemWrite, MemRead, Beq, Bne, Jump, MemToReg, RegWrite);
+	//Input 
 	input [5:0] opcode;
+	
+	//Output 
 	output ALUsrc, RegDst, MemWrite, MemRead, Beq, Bne, Jump, MemToReg, RegWrite;
 	output [1:0] ALUop;
 
-	reg RFormat, lw, sw, Beq, Bne, Jump;
+	//Temp variables 
+	reg RFormat, lw, sw, beq, bne, jmp; 
 
-	always @* begin
-		case (opcode)
-			6'b100011: assign lw = 1'b1;
-			6'b101011: assign sw = 1'b1;
-			6'b000100: assign Beq = 1'b1;
-			6'b111011: assign Bne = 1'b1;
-			6'b100001: assign Jump = 1'b1;
-			default: assign RFormat = 1'b1;
-		endcase
+	reg ALUsrc, RegDst, MemWrite, MemRead, Beq, Bne, Jump, MemToReg, RegWrite;
+	reg [1:0] ALUop;
+
+	always@(opcode)
+	begin
+	  assign RFormat = (~opcode[0] & ~opcode[1] & ~opcode[2] & ~opcode[3] & ~opcode[4] & ~opcode[5]);
+	  assign lw = (opcode[0] & ~opcode[1] & ~opcode[2] & ~opcode[3] & opcode[4] & opcode[5]);
+	  assign sw = (opcode[0] & ~opcode[1] & opcode[2] & ~opcode[3] & opcode[4] & opcode[5]);
+	  assign beq = (~opcode[0] & ~opcode[1] & ~opcode[2] & opcode[3] & ~opcode[4] & ~opcode[5]);
+	  assign bne = (opcode[0] & opcode[1] & opcode[2] & ~opcode[3] & opcode[4] & opcode[5]);
+	  assign jmp = (opcode[0] & ~opcode[1] & ~opcode[2] & ~opcode[3] & ~opcode[4] & opcode[5]);
+
+	  assign ALUsrc = (lw | sw); 
+	  assign RegDst = RFormat; 
+	  assign MemWrite = sw; 
+	  assign MemRead = lw; 
+	  assign Beq = beq;
+	  assign Bne = bne; 
+	  assign Jump = jmp; 
+	  assign MemToReg = lw; 
+	  assign RegWrite = (RFormat | lw);
+	  assign ALUop[0] = (bne | beq | jmp);
+	  assign ALUop[1] = (RFormat | jmp);
 	end
 
-	assign RegDst = RFormat;
-	assign ALUsrc = lw | sw;
-	assign MemToReg = lw;
-	assign RegWrite = RFormat | lw; 
-	assign MemRead = lw; 
-	assign MemWrite = sw; 
-	assign ALUop[1] = RFormat; 
-	assign ALUop[0] = Beq | Bne | Jump; 
 endmodule
