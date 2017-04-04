fd26fa9a32eed8601265fbff45ab64c4a31ed5e1
JTEnglish <je787413@ohio.edu>
2017-04-04 17:53:21 -0400

    reg and mem ready for waveform

--- HW5Module/RegisterFile/RegisterFile_tb.v
+++ HW5Module/RegisterFile/RegisterFile_tb.v
@@ -1,4 +1,4 @@
-module Mux5Bit2to1test();
+module RegisterFile_tb();
 	//Inputs
   reg [4:0] ReadRegister1, ReadRegister2; // Two registers to be read
   reg [4:0] WriteRegister; // Register address to write into
@@ -13,8 +13,8 @@ module Mux5Bit2to1test();
 		$dumpfile("wave");
 		$dumpvars(0, test);
 
-		$display("time\tReadRegister1\tReadRegister2\tWriteRegister\tWriteData\tRegWrite\tClk\tReadData1\tReadData2");
-		$monitor("%g\t%b\t%b\t%b\t%b\t%b\t%b\t%b\t%b",
+		$display("time\tReadRegister1\tReadRegister2\tWriteRegister\tWriteData\t\t\t\tRegWrite\tClk\tReadData1\tReadData2");
+		$monitor("%g\t%b\t\t%b\t\t%b\t\t%b\t%b\t%b\t%b\t%b",
 		$time, ReadRegister1, ReadRegister2, WriteRegister, WriteData, RegWrite, Clk, ReadData1, ReadData2);
 
     Clk = 1;
@@ -28,8 +28,8 @@ module Mux5Bit2to1test();
     WriteData = 32'b10000000000000000000000001111111;
     RegWrite = 1;
 
-		// Wait 10 s for global reset to finish
-		#10;
+		// Wait 20 s for global reset to finish
+		#20;
 
 		///////////////////
 		//TEST 2
@@ -40,7 +40,19 @@ module Mux5Bit2to1test();
     WriteData = 32'b10000000000000000000000001111111;
     RegWrite = 0;
 
-		#10 $finish;
+    // Wait 20 s for global reset to finish
+		#20;
+
+		///////////////////
+		//TEST 3
+		// Initialize Inputs
+    ReadRegister1 = 5'b10011;
+    ReadRegister2 = 5'b11010;
+    WriteRegister = 5'b10011;
+    WriteData = 32'b11011000001111110000000000111111;
+    RegWrite = 1;
+
+		#20 $finish;
 	end
 
   always begin
